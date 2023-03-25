3413d1edddfe90c741ceb9082f2853537f3eb95d
Roming22 <roming22@gmail.com>
2023-03-25 19:22:02 -0400

    Import package installed with pip

--- src/main.py
+++ src/main.py
@@ -5,15 +5,26 @@ A more thorough description of the application, which could include:
     - Various examples of how to run the command.
     - Warning about undesirable side effects.
 """
+import click
 
 from utils.prime import is_prime
 
 
-def main() -> None:
+@click.command(
+    help=__doc__,
+)
+@click.option(
+    "-c",
+    "--count",
+    help="How many prime must be found before stopping",
+    default=5000,
+    type=int,
+)
+def main(count: int) -> None:
     print(f"Hello World!")
     primes = []
     n = 1
-    while len(primes) < 6000:
+    while len(primes) < count:
         n += 1
         if is_prime(n):
             primes.append(n)
